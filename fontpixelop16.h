5dd6e1a369c9635fdc57732fdabfe2f6d63282de
Sahil Raut <sahilvraut777@gmail.com>
2024-09-06 11:56:08 +0000

    Update fontpixelop16.h

--- fontpixelop16.h
+++ fontpixelop16.h
@@ -1,25 +1,5 @@
-/*
- * Tiny4kOLED - Drivers for SSD1306 controlled dot matrix OLED/PLED 128x32 displays
- *
- * Based on ssd1306xled, re-written and extended by Stephen Denne
- * from 2017-04-25 at https://github.com/datacute/Tiny4kOLED
- *
- */
-/*
- * SSD1306xLED - Drivers for SSD1306 controlled dot matrix OLED/PLED 128x64 displays
- *
- * @created: 2014-08-12
- * @author: Neven Boyanov
- *
- * Source code available at: https://bitbucket.org/tinusaur/ssd1306xled
- *
- */
-
-// ----------------------------------------------------------------------------
-
 #include <avr/pgmspace.h>
 
-// ----------------------------------------------------------------------------
 
 /* font */
 const uint8_t Tiny4kOLED_pixelop16 [] PROGMEM = {
